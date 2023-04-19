f762814c902175e6ae4fdf85a321c0710521cedf
aaaawheee <raviprajapati4925@gmail.com>
2023-04-19 16:06:00 -0400

    Solved Errors with LoginPage & Registration. Setup the basic need for Login & Registration.

--- ats-frontend/src/app/app.module.ts
+++ ats-frontend/src/app/app.module.ts
@@ -8,11 +8,14 @@ import { RegisterComponent } from './components/register/register.component';
 import { AngularMaterialModule } from './angular-material.module';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { LogInComponent } from './components/log-in/log-in.component';
+import { MatFormField, MatFormFieldModule } from '@angular/material/form-field';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RegisterComponent
+    RegisterComponent,
+    LogInComponent
   ],
   imports: [
     BrowserModule,
@@ -21,7 +24,8 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     AngularMaterialModule,
     FlexLayoutModule ,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    MatFormFieldModule
   ],
   providers: [],
   bootstrap: [AppComponent],
