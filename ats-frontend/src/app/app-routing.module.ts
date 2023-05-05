07eb3d3952a81c7dbd7718bb12b2cb3cbcd90d52
shubh dungarani <sdungara@nyit.edu>
2023-05-05 13:15:45 -0400

    resume evaluation works

--- ats-frontend/src/app/app-routing.module.ts
+++ ats-frontend/src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { LogInComponent } from './components/log-in/log-in.component';
 import { RegisterComponent } from './components/register/register.component';
 import { HomepageComponent } from './components/homepage/homepage.component';
 const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: 'login' },
+  { path: '', pathMatch: 'full', redirectTo: 'homepage' },
   { path: 'login', component: LogInComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'homepage', component: HomepageComponent }
