4b37d9c260adce45f743b5874b5e2a8b3b15c0f4
shubh dungarani <sdungara@nyit.edu>
2023-05-05 15:16:45 -0400

    Bot Module

--- ats-frontend/src/app/components/chatgpt/chatgpt.component.ts
+++ ats-frontend/src/app/components/chatgpt/chatgpt.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
 
 interface Option{
   value: string;
@@ -11,11 +14,31 @@ interface Option{
   styleUrls: ['./chatgpt.component.scss']
 })
 export class ChatgptComponent {
+  JobsDone = false;
+  myForm = new FormGroup({
+    variable: new FormControl('', [Validators.required])
+  });
+
+
     selectedValue: string | undefined;
-    jobTitle: string | undefined;
+    variable: string | undefined;
     options: Option[] = [
       {value: 'ResumeOptimising', viewValue: 'Need suggestions for optimising your Resume?'},
       {value: 'Skills Required', viewValue: 'Need skills suggestion for any particular job?'},
     ];
+    public Jobs : any = [];
 
+    constructor(private http: HttpClient) { }
+    
+    getMessage(jobTitle: any){
+          let data = {'jobtitle': jobTitle};
+          this.http.post('http://localhost:4000/chatgpt', data)
+            .subscribe(res => {
+              console.log(this.Jobs.data)
+                this.JobsDone = true;
+                this.Jobs = res;
+                console.log(this.Jobs);
+              })
+        }
+      
   }
